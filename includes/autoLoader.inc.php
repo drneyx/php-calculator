bdf79a5c77d8380ef25979a4da277140a01a176b
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-08 12:25:36 +0300

    Add autoloader class

--- includes/autoLoader.inc.php
+++ includes/autoLoader.inc.php
@@ -2,13 +2,16 @@
 spl_autoload_register('myAutoLoader'); 
 
 function myAutoLoader($className){
-    $path = "classes/";
-    $ext = ".class.php";
-    $fullPath = $path . str_replace('\\', '/', $className) . $ext;
-    
-    if(!file_exists($fullPath)){
-        return False;
+    $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+
+    if(strpos($url, 'includes') !== false){
+        $path = '../classes/';
     }
 
-    include_once $fullPath;
+    else{
+        $path = 'classes';
+    }
+    $ext = ".class.php";
+    require_once $path . $className . $ext;
 }
